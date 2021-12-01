be197b79986046c1d75c999897b3c3d772412283
Yuya Oiwa <yuya.oiwa@me.com>
2021-12-02 08:45:42 +0900

    remove unsued code

--- src/pages/collection/[address]/[tokenId].tsx
+++ src/pages/collection/[address]/[tokenId].tsx
@@ -1,20 +1,3 @@
-/*
-  This example requires Tailwind CSS v2.0+ 
-  
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/typography'),
-      require('@tailwindcss/aspect-ratio'),
-    ],
-  }
-  ```
-*/
 import * as React from "react";
 import { Disclosure } from "@headlessui/react";
 import {
