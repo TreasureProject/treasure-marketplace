3111128fb9b4b165bd53787b6d9a3a0b608ed272
Yuya Oiwa <yuya.oiwa@me.com>
2021-12-05 09:25:35 +0900

    wording

--- src/pages/collection/[address]/[tokenId].tsx
+++ src/pages/collection/[address]/[tokenId].tsx
@@ -415,7 +415,7 @@ export default function Example() {
                                       scope="col"
                                       className="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider hidden lg:table-cell"
                                     >
-                                      Expiration
+                                      Expire In
                                     </th>
                                     <th
                                       scope="col"
