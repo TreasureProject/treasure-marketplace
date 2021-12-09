93888503c6f78a806088ba175e11c550924dcbf5
Yuya Oiwa <yuya.oiwa@me.com>
2021-12-09 11:27:41 +0900

    unrelated but added link to details from activity

--- src/graphql/queries.graphql.ts
+++ src/graphql/queries.graphql.ts
@@ -154,11 +154,15 @@ const LISTING_FRAGMENT = gql`
       id
     }
     token {
+      tokenId
       metadata {
         description
         image
       }
       name
+    }
+    collection {
+      id
     }
     transactionLink
   }
